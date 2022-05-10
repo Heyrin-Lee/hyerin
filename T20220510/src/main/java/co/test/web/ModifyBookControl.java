e47a54b6390191696c45b7f61a43171663523fdc
hyerin <zzzang1302@naver.com>
2022-05-10 18:31:15 +0900

    0510

---
+++ T20220510/src/main/java/co/test/web/ModifyBookControl.java
@@ -0,0 +1,38 @@
+package co.test.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import co.test.service.BookService;
+import co.test.vo.BookVO;
+
+public class ModifyBookControl implements Controller {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String code = request.getParameter("code");
+		String title = request.getParameter("title");
+		String author = request.getParameter("author");
+		String press = request.getParameter("press");
+		int price = Integer.parseInt(request.getParameter("price"));
+		
+		BookVO vo = new BookVO();
+		vo.setBookCode(code);
+		vo.setBookTitle(title);
+		vo.setBookAuthor(author);
+		vo.setBookPress(press);
+		vo.setBookPrice(price);
+		
+		BookService service = new BookService();
+		service.modifyBook(vo);
+		
+		
+		
+		request.getRequestDispatcher("result/modifyOutput.jsp").forward(request, response);
+	}
+
+}
